37ecdd7f08ab4e41ba615252758e376734775580
Anish Baniya <anishbaniya07@gmail.com>
2023-10-06 11:20:54 +0545

    this is first

---
+++ 2nd.dart
@@ -0,0 +1,7 @@
+void main(){
+    String name = "Anish";
+    String address = "Pokhara";
+    DateTime dob = DateTime (2002,2,12);  
+print("My name is ${name}. I'm from ${address}.\n My dob is ${dob.year}");
+
+}
